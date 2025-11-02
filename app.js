bb7146e07076d44562bdc762b6e0dcd91f1815a6
Mohammed Faisal Iqbal <142092910+faisaliqbal-dev@users.noreply.github.com>
2025-11-02 19:06:42 +0530

    Update app.js

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ const port = process.env.PORT || 3000;
 
 // Home route
 app.get('/', (req, res) => {
-  res.send('🚀 Hello from AWS DevOps Demo Applications from faisal!');
+  res.send('🚀 Hello from AWS DevOps Demo Applications from faisal iqbal !');
 });
 
 // Healthcheck route (useful for load balancer / k8s)
