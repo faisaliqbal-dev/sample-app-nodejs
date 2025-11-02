f4c00ba89d9c8e425a7af4a16f4a0c1ca1cb9f25
Mohammed Faisal Iqbal <142092910+faisaliqbal-dev@users.noreply.github.com>
2025-11-02 18:55:39 +0530

    Update app.js

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ const port = process.env.PORT || 3000;
 
 // Home route
 app.get('/', (req, res) => {
-  res.send('🚀 Hello from AWS DevOps Demo Applicationssss!');
+  res.send('🚀 Hello from AWS DevOps Demo Applications!');
 });
 
 // Healthcheck route (useful for load balancer / k8s)
